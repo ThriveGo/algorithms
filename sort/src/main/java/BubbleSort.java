944b42308fce8910ce01c6109305fe02390b3dcc
rong.fan <rong.fan@baozun.com>
2018-03-01 16:31:38 +0800

    add

--- sort/src/main/java/BubbleSort.java
+++ sort/src/main/java/BubbleSort.java
@@ -2,4 +2,20 @@
  * @author jack
  */
 public class BubbleSort {
+    public void sort(int[] a)
+    {
+        int temp = 0;
+        for (int i = a.length - 1; i > 0; --i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                if (a[j + 1] < a[j])
+                {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
